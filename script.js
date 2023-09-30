33ea2c658a1f30835bb79abfbfeb89e67bd7e7d9
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-30 11:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const audio = document.getElementById('audio');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const soundFile = button.getAttribute('data-sound');
+        if (soundFile) {
+            audio.src = `sounds/${soundFile}`;
+            audio.play();
+        }
+    });
+});
+
+stopButton.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+});
